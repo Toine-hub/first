09aaf9841ca05931e932c0be14cfcafcf32934ce
Tonio-hub <ravenetantoine@gmail.com>
2025-09-24 22:53:48 +0200

    suite

--- src/M1E5.java
+++ src/M1E5.java
@@ -10,6 +10,11 @@ public class M1E5  {
         // Génère un nombre aléatoire entre 1 et 100 inclus
         int nombreMystere = random.nextInt(100) + 1;
 
-        // 
+        // Scanner pour lire la saisie utilisateur
+        Scanner clavier = new Scanner(System.in);
+
+        // Demande la saisie de l'utilisateur
+        System.out.println("Je pense à un nombre entre 1 et 100 inclus. Devinez lequel.");
+        System.out.print("Entrez un nombre : ");
     }
 }
