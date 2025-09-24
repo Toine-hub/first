f42aeba16a2067caa5692674bf108d90ea1cacb7
Tonio-hub <ravenetantoine@gmail.com>
2025-09-24 22:44:30 +0200

    1ere partie

---
+++ src/M1E5.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class M1E5  {
+    public static void main(String[] args) {
+
+        // Création d'un générateur de nombre aléatoires
+        Random random = new Random();
+        
+        // Génère un nombre aléatoire entre 1 et 100 inclus
+        int nombreMystere = random.nextInt(100) + 1;
+
+        // 
+    }
+}
