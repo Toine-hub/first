6ee54b78136558f4dc3eeb2e5ee0766a09c88bbb
Tonio-hub <ravenetantoine@gmail.com>
2025-09-25 16:59:16 +0200

    Exercice 1 du module 2 terminé

---
+++ src/M2E1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class M2E1 {
+    public static void main(String[] args) {
+
+        // Ouverture de la ressource clavier
+        Scanner clavier = new Scanner(System.in);
+
+        // Demande de saisie utilisateur
+        System.out.print("Entrez un nombre : ");
+        int nombre = clavier.nextInt();
+
+        if (nombre % 2 == 0) {
+            System.out.println(nombre + " est pair.");
+        } else {
+            System.out.println(nombre + " est impair.");
+        }
+
+    }
+}
