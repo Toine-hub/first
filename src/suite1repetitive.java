1a4caadd7df8584c89251344df95d1d745f49af0
Tonio-hub <ravenetantoine@gmail.com>
2025-10-16 15:49:09 +0200

    Suite 1 structures répétitives

---
+++ src/suite1repetitive.java
@@ -0,0 +1,12 @@
+public class suite1repetitive {
+    public static void main(String[] args) {
+
+        int nb = -4;
+
+        while (nb <= 86) {
+            System.out.print(nb + " ");
+            nb = nb + 18;
+        }
+
+    }
+}
