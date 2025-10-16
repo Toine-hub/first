092f10a546d94b9debc72629b21be3d312c06a87
Tonio-hub <ravenetantoine@gmail.com>
2025-10-16 16:07:04 +0200

    Motif 2 du module 3

---
+++ src/M3motif2.java
@@ -0,0 +1,10 @@
+public class M3motif2 {
+    public static void main(String[] args) {
+
+        String str = "*****";
+
+        for (int i = 1; i <= 4; i++) {
+            System.out.println(str);
+        }
+    }
+}
